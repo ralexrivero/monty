b05ea133b3c7700415580d24b6724e44db2eba18
ralexrivero <ralexrivero@gmail.com>
2021-09-22 18:09:00 -0300

    stage 01

---
+++ opcodes.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+int opcode_search(const char *opcode)
+{
+
+opcode_t codes[] = {
+    {"push", push},
+    {"pall", pall},
+
+    {NULL, NULL}
+};
+
+int size = sizeof(codes) / sizeof(opcode_t) - 1;
+
+while (i < size)
+{
+    if (codes[])
+}
+return (EXIT_SUCCESS);
+}
