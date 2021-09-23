c54b9d698bbb67627893a1af27e432d74e79354e
Andres <3283@holbertonschool.com>
2021-09-23 16:23:56 -0700

    Added free function

--- m_freestack.c
+++ m_freestack.c
@@ -1,2 +1,17 @@
 #include "monty.h"
+/**
+ * free_stack - Function to free the stack
+ * @stack: pointer to the stack to be freed
+ * Return: nothing
+ */
+void free_stack(stack_t **stack)
+{
+	stack_t *tmp;
 
+	while (*stack != NULL)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		free(tmp);
+	}
+}
