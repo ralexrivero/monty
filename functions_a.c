a790a88c5bd337200ed32bf4950a811a31010eab
Andres <3283@holbertonschool.com>
2021-09-22 19:48:13 -0700

    modified functions

--- functions_a.c
+++ functions_a.c
@@ -1,41 +1,42 @@
 #include "monty.h"
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
- * @head: position to add node
- * @n: node data
+ * push - Adds a new node at the beginning of a stack_t list.
+ * @stack: pointer to stack
+ * @line: line number
  * Return: The address of the new element, or NULL if it failed
  */
-stack_t *push(dlistint_t **head, unsigned int n) /*push*/
+void push(stack_t **stack, unsigned int line) 
 {
 	stack_t *new_node;
 
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
-		return (NULL);
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	new_node->n = n;
-	new_node->next = *head;
+	new_node->next = *stack;
 	new_node->prev = NULL;
-	if (*head)
-		(*head)->prev = new_node;
-	*head = new_node;
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 	return (new_node);
 }
 
 /**
- * print_dlistint - Function that prints all the elements of a dlistint_t list.
- * @h: node
+ * pall - Function that prints all the elements of a stack_t list.
+ * @stack
  * Return: Number of nodes
  */
-size_t pall(const stack_t *h) /*pall*/
+size_t pall(const stack_t *stack) 
 {
 	size_t num_of_nodes = 0;
 
-	while (h != NULL)
+	while (stack)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", stack->n);
 		num_of_nodes++;
-		h = h->next;
+		stack = stack->next;
 	}
 	return (num_of_nodes);
 }
@@ -49,30 +50,28 @@ void pint(stack_t **stack, unsigned int line)
 	int value;
 	if(!stack || !*stack)
 	{
-		printf("L<line_number>: can't pint, stack empt\n", line);
+		fprintf(stderr, "L<line_number>: can't pint, stack empt\n", line);
 		exit(EXIT_FAILURE);
 	}
-	value = (*stack*)->n;
+	value = (*stack)->n;
 	printf("%d\n",value);
 }
 
 /**
- * delete_dnodeint_at_index - deletes the node at index
-	*	of a dlistint_t linked list
- * @head: A pointer to the head of the dlistint_t.
- * @index: index of the node that should be deleted. Index starts at 0
- * Return: 1 if it succeeded, -1 if it failed
+ * pop - deletes the node at the top of te stack
+ * @stack: A pointer to the head of the stack_t.
+ * @line: index of the node that should be deleted.
+ * Return: -1 if it failed
  */
-int delete_dnodeint_at_index(stack_t **head, unsigned int index) /*pop- specify positiion*/
+void pop(stack_t **stack, unsigned int line) 
 {
-	stack_t *tmp = *head;
-
-	if (*head == NULL)
-		return (-1);
-	for (; index != 0; index--)
+	stack_t *tmp = *stack
++
+	if (*stack == NULL || stack == NULL)
 	{
-		if (tmp == NULL)
-			return (-1);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line)
+		exit(EXIT_FAILURE):
+	}
 		tmp = tmp->next;
 	}
 	if (tmp == *head)
