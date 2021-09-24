7fbdde8f5a9bb4a09b8f5eec3fd4316eee7296ca
Andres <3283@holbertonschool.com>
2021-09-23 17:13:42 -0700

    added swap and nop

---
+++ functions_b.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/** swap - swaps the top two elements of the stack
+ * @stack: pointer to the head of stack
+ * @line: line number
+ * Return: Nothing on success, EXIT_FALURE if failure
+ */
+void swap(stack_t **stack, unsigned int line)
+{
+	int first_tmp, second_tmp;
+
+	if ((*stack)->next == NULL || (!*stack))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	first_tmp = (*stack)->n;
+	second_tmp = (*stack)->next->n;
+
+	(*stack)->n = second_tmp;
+	(*stack)->next->n = first_tmp;
+}
+/**
+ * nop - does nothing
+ * @stack: pointer to stack
+ * @line: line number
+ */
+void nop(stack_t **stack, unsigned int line)
+{
+	(void)stack;
+	(void)line;
+}
+
