aec78936cc70ce0db4de38ed99ea6051d9a3787d
ralexrivero <ralexrivero@gmail.com>
2021-09-26 12:21:04 -0400

    POSIX 200809L fix

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#gcc -Wall -Werror -Wextra -pedantic -std=c90 *.c -o monty
-gcc -Wall -Werror -Wextra -pedantic *.c -o monty
+gcc -Wall -Werror -Wextra -pedantic -std=c90 *.c -o monty
--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define _POSIX_C_SOURCE 200809L
 
 /* libraries */
 #include <stdio.h>
